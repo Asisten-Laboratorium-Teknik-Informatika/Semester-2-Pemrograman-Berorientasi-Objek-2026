f29ec3083f87d93d27f5bef8692708b62bf1dd3b
Cynthia Samosir <crazyblabla73@gmail.com>
2026-05-28 14:10:48 +0700

    Add files via upload

---
+++ B1_Sistem Informasi Akademik Kampus_kelompok1/projek akhir/Fitur/Main.java
@@ -0,0 +1,15 @@
+package Fitur;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("============================================================");
+        System.out.println(" SISTEM INFORMASI AKADEMIK UNIVERSITAS SUMATERA UTARA ");
+        System.out.println("============================================================");
+
+        Login lg = new Login();
+        lg.loginUser();
+
+    }
+}
---
+++ B1_Sistem Informasi Akademik Kampus_kelompok1/projek akhir/Fitur/Session.java
@@ -0,0 +1,12 @@
+package Fitur;
+
+public class Session {
+    public static String username;
+    public static String password;
+    public static String role;
+    public static String nama;
+    public static String nim;
+    public static String nidn;
+    public static String tahun;
+    public static String semester_aktif;
+}
