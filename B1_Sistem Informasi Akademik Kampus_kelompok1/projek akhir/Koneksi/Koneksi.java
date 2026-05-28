9099b3f43648e963b5177760d466b9890e00929f
Cynthia Samosir <crazyblabla73@gmail.com>
2026-05-28 14:11:43 +0700

    Add files via upload

---
+++ B1_Sistem Informasi Akademik Kampus_kelompok1/projek akhir/Koneksi/Koneksi.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Koneksi {
+    public static Connection connect(){
+        try {
+            String url = "jdbc:postgresql://localhost:5432/sistem_akademi_kampus";
+            String user = "postgres";
+            String password = "postgres";
+            
+            Connection conn = DriverManager.getConnection(url, user, password);
+            return conn;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
