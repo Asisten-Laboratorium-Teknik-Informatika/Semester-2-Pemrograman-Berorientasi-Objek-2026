ad1e7330864b807016cd8fef74d5f2563446fd7c
pablo_olbap <pablomanalu29@gmail.com>
2026-06-02 20:48:12 +0700

    Update PengirimanDAO.java

--- B1_Sistem Penjualan dan Distribusi Barang_Kelompok 3/projekakhir_v4/dao/PengirimanDAO.java
+++ B1_Sistem Penjualan dan Distribusi Barang_Kelompok 3/projekakhir_v4/dao/PengirimanDAO.java
@@ -29,5 +29,41 @@ public class PengirimanDAO {
             ps.setString(2, idPengiriman);
             ps.executeUpdate();
         }
+    } 
+    
+    public void tampilkanLaporanLogistikLengkap() throws SQLException {
+        String sql = "SELECT pg.id_pengiriman, p.id_penjualan, pl.nama AS nama_pelanggan, " +
+                    "k.nama_kurir, pg.tanggal_kirim, pg.status_pengiriman " +
+                    "FROM pengiriman pg " +
+                    "JOIN penjualan p ON pg.id_penjualan = p.id_penjualan " +
+                    "JOIN pelanggan pl ON p.id_pelanggan = pl.id_pelanggan " +
+                    "JOIN kurir k ON pg.id_kurir = k.id_kurir";
+
+        try (Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(sql)) {
+            System.out.println("\n=========================================================================================");
+            System.out.println("                           LAPORAN OPERASIONAL LOGISTIK & DISTRIBUSI                     ");
+            System.out.println("=========================================================================================");
+            System.out.printf("%-10s | %-10s | %-20s | %-15s | %-12s | %-10s\n", 
+                "ID KIRIM", "ID PENJ", "NAMA PELANGGAN", "KURIR VENDOR", "TGL KIRIM", "STATUS");
+            System.out.println("-----------------------------------------------------------------------------------------");
+            
+            boolean adaData = false;
+            while (rs.next()) {
+                adaData = true;
+                System.out.printf("%-10s | %-10s | %-20s | %-15s | %-12s | %-10s\n",
+                    rs.getString("id_pengiriman"),
+                    rs.getString("id_penjualan"),
+                    rs.getString("nama_pelanggan"),
+                    rs.getString("nama_kurir"),
+                    rs.getDate("tanggal_kirim").toString(),
+                    rs.getString("status_pengiriman")
+                );
+            }
+            
+            if (!adaData) {
+                System.out.println("Belum ada pengiriman barang yang tercatat.");
+            }
+            System.out.println("=========================================================================================");
+        }
     }
 }
