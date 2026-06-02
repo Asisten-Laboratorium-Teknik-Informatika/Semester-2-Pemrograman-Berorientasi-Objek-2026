e86385f84037c342b927f47f8af7a936f5f3da46
Geraldo Stevensius <geralsit22@gmail.com>
2026-06-02 23:49:21 +0700

    Delete B1_Sistem Manajemen SDM, Fokus Pegawai & payroll_KELOMPOK 7 directory

--- B1_Sistem Manajemen SDM, Fokus Pegawai & payroll_KELOMPOK 7/PROJECTAKHIR/src/config/DatabaseConfig.java
+++
@@ -1,9 +0,0 @@
-package config;
-import java.sql.*;
-
-public class DatabaseConfig {
-    public static Connection getConnection() throws SQLException {
-        String url = "jdbc:postgresql://localhost:5432/PROJECTAKHIR";
-        return DriverManager.getConnection(url, "postgres", "12345");
-    }
-}
