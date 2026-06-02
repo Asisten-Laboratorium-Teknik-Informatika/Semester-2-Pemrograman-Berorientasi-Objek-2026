b3c7918fed2cfca635bdf8c26e9de7d8789dd47a
Cynthia Samosir <crazyblabla73@gmail.com>
2026-06-02 14:04:34 +0700

    Update Koneksi.java

--- B1_Sistem Informasi Akademik Kampus_kelompok1/projek akhir/Koneksi/Koneksi.java
+++ B1_Sistem Informasi Akademik Kampus_kelompok1/projek akhir/Koneksi/Koneksi.java
@@ -4,6 +4,7 @@ import java.sql.DriverManager;
 public class Koneksi {
     public static Connection connect(){
         try {
+            Class.forName("org.postgresql.Driver");
             String url = "jdbc:postgresql://localhost:5432/sistem_akademi_kampus";
             String user = "postgres";
             String password = "postgres";
