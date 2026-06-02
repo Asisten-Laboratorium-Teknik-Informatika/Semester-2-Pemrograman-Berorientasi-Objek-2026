1eaa300a1263e8e275efa0aaf3592f96845abf16
pablo_olbap <pablomanalu29@gmail.com>
2026-06-02 20:49:26 +0700

    Update PenjualanDAO.java

--- B1_Sistem Penjualan dan Distribusi Barang_Kelompok 3/projekakhir_v4/dao/PenjualanDAO.java
+++ B1_Sistem Penjualan dan Distribusi Barang_Kelompok 3/projekakhir_v4/dao/PenjualanDAO.java
@@ -53,4 +53,58 @@ public class PenjualanDAO {
         }
         return 0.0;
     }
+
+    public void cetakNotaTransaksi(String idPenjualan) throws SQLException {
+        String sqlHeader = "SELECT p.id_penjualan, p.tanggal_penjualan, p.total_bayar, " +
+                        "pl.nama AS nama_pelanggan, pl.id_pelanggan, k.nama AS nama_kasir, k.id_karyawan " +
+                        "FROM penjualan p " +
+                        "LEFT JOIN pelanggan pl ON p.id_pelanggan = pl.id_pelanggan " +
+                        "LEFT JOIN karyawan k ON p.id_karyawan = k.id_karyawan " +
+                        "WHERE p.id_penjualan = ?";
+
+        String sqlItems = "SELECT dp.jumlah, dp.subtotal, pr.nama_produk, pr.harga " +
+                        "FROM detail_penjualan dp " +
+                        "JOIN produk pr ON dp.id_produk = pr.id_produk " +
+                        "WHERE dp.id_penjualan = ?";
+
+        try (PreparedStatement psHeader = conn.prepareStatement(sqlHeader)) {
+            psHeader.setString(1, idPenjualan);
+            try (ResultSet rsH = psHeader.executeQuery()) {
+                if (rsH.next()) {
+                    System.out.println("\n==================================================");
+                    System.out.println("                  InforEn Fashion                 ");
+                    System.out.println("==================================================");
+                    System.out.printf("ID Nota   : %s\n", rsH.getString("id_penjualan"));
+                    System.out.printf("Tanggal   : %s\n", rsH.getTimestamp("tanggal_penjualan").toString());
+                    System.out.printf("Pelanggan : %s (%s)\n", rsH.getString("nama_pelanggan"), rsH.getString("id_pelanggan"));
+                    System.out.printf("Kasir     : %s (%s)\n", rsH.getString("nama_kasir"), rsH.getString("id_karyawan"));
+                    System.out.println("--------------------------------------------------");
+                    System.out.printf("%-20s %-5s %-10s %-12s\n", "Produk", "Qty", "Harga", "Subtotal");
+                    System.out.println("--------------------------------------------------");
+
+                    try (PreparedStatement psItems = conn.prepareStatement(sqlItems)) {
+                        psItems.setString(1, idPenjualan);
+                        try (ResultSet rsI = psItems.executeQuery()) {
+                            while (rsI.next()) {
+                                System.out.printf("%-20s %-5d Rp%-9,.0f Rp%,.0f\n", 
+                                    rsI.getString("nama_produk"),
+                                    rsI.getInt("jumlah"),
+                                    rsI.getDouble("harga"),
+                                    rsI.getDouble("subtotal")
+                                );
+                            }
+                        }
+                    }
+                    
+                    System.out.println("--------------------------------------------------");
+                    System.out.printf("TOTAL BELANJA                       : Rp%,.2f\n", rsH.getDouble("total_bayar"));
+                    System.out.println("==================================================");
+                    System.out.println("         -- Terima Kasih Telah Berbelanja --\n");
+                    
+                } else {
+                    System.out.println(">> Error: ID Transaksi " + idPenjualan + " tidak ditemukan di database.");
+                }
+            }
+        }
+    }
 }
